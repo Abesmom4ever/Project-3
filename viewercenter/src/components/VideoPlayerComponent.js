be932daba7b49a5ca222a3da107a7fe98098a6ac
Armando Ugalde <uarmando15@yahoo.com>
2022-09-02 18:10:36 -0700

    added css styles to vid

--- viewercenter/src/components/VideoPlayerComponent.js
+++ viewercenter/src/components/VideoPlayerComponent.js
@@ -1,8 +1,26 @@
 import { useRef } from 'react';
 import useVideoPlayer from './videoplayer'
 import video from "../temp video/Alex Jones will hunt down the enemies of Infowars.mp4"
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#d50000',
+    },
+    },
+});
 
 export function VideoPlayerComponent () {
+ 
     const videoElement = useRef(null);
     const {
       playerState,
@@ -14,6 +32,17 @@ export function VideoPlayerComponent () {
       handleVolumeChange,
       VideoInfo,
     } = useVideoPlayer(videoElement);
+      function SliderSizes() {
+  return (
+    <Box width={300}>
+      <Slider 
+      defaultValue={0}
+      aria-label="Default"
+      value={playerState.progress}
+      onChange={(e) => handleVideoProgress(e)} />
+    </Box>
+  );
+}
     return (
         <div className="container">
       <div className="video-wrapper">
@@ -24,21 +53,31 @@ export function VideoPlayerComponent () {
         />
         <div className="controls">
           <div className="actions">
+            <ThemeProvider theme={theme}>
             <button onClick={togglePlay}>
               {!playerState.isPlaying ? (
-                <i className="bx bx-play"></i>
+                <i className="bx bx-play"> <PlayArrowIcon/></i>
               ) : (
-                <i className="bx bx-pause"></i>
+                <i className="bx bx-pause"><PauseIcon/></i>
               )}
             </button>
+            </ThemeProvider>
           </div>
-          <input
+          <SliderSizes
+          type="range"
+          min="0"
+          max="100"
+          value={playerState.progress}
+          onChange={(e) => handleVideoProgress(e)}
+          />
+          {/* <input
             type="range"
             min="0"
             max="100"
             value={playerState.progress}
             onChange={(e) => handleVideoProgress(e)}
-          />
+            
+          /> */}
           <select
             className="velocity"
             value={playerState.speed}
@@ -51,18 +90,18 @@ export function VideoPlayerComponent () {
           </select>
           <button className="mute-btn" onClick={toggleMute}>
             {!playerState.isMuted ? (
-              <i className="bx bxs-volume-full"></i>
+              <i className="bx bxs-volume-full"><VolumeUpIcon/></i>
             ) : (
-              <i className="bx bxs-volume-mute"></i>
+              <i className="bx bxs-volume-mute"><VolumeOffIcon/></i>
             )}
           </button>
-          <input
+          {/* <input
           type="range"
           min="0"
           max="100"
           value={playerState.volume}
           onChange={(e) => handleVolumeChange(e)}
-        />
+        /> */}
         </div>
       </div>
     </div>
